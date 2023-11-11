e46041dbc7a10c7738fe2c80ec63e26be50b433f
payakorn_sak@cmu.ac.th <payakornn@gmail.com>
2023-11-11 21:42:31 +0700

    add files

--- src/script.jl
+++ src/script.jl
@@ -9,21 +9,21 @@ println("number of Threads: $(Threads.nthreads())")
 # Simulated Annealing 
 for i in [100]
     
-    simulated_annealing_run(obj_func=VRPTW.balancing_value_weighted_sum_w10_w0, num_node=i)
-    date_finish = now()
-    println("end program W10-0: $(Dates.format(date_finish, "e, d u yyyy H:M:S"))")
+    # simulated_annealing_run(obj_func=VRPTW.balancing_value_weighted_sum_w10_w0, num_node=i)
+    # date_finish = now()
+    # println("end program W10-0: $(Dates.format(date_finish, "e, d u yyyy H:M:S"))")
     
-    simulated_annealing_run(obj_func=VRPTW.balancing_value_weighted_sum_w9_w1, num_node=i)
-    date_finish = now()
-    println("end program W9-1: $(Dates.format(date_finish, "e, d u yyyy H:M:S"))")
+    # simulated_annealing_run(obj_func=VRPTW.balancing_value_weighted_sum_w9_w1, num_node=i)
+    # date_finish = now()
+    # println("end program W9-1: $(Dates.format(date_finish, "e, d u yyyy H:M:S"))")
     
-    simulated_annealing_run(obj_func=VRPTW.balancing_value_weighted_sum_w8_w2, num_node=i)
-    date_finish = now()
-    println("end program W8-2: $(Dates.format(date_finish, "e, d u yyyy H:M:S"))")
+    # simulated_annealing_run(obj_func=VRPTW.balancing_value_weighted_sum_w8_w2, num_node=i)
+    # date_finish = now()
+    # println("end program W8-2: $(Dates.format(date_finish, "e, d u yyyy H:M:S"))")
     
-    simulated_annealing_run(obj_func=VRPTW.balancing_value_weighted_sum_w7_w3, num_node=i)
-    date_finish = now()
-    println("end program W7-3: $(Dates.format(date_finish, "e, d u yyyy H:M:S"))")
+    # simulated_annealing_run(obj_func=VRPTW.balancing_value_weighted_sum_w7_w3, num_node=i)
+    # date_finish = now()
+    # println("end program W7-3: $(Dates.format(date_finish, "e, d u yyyy H:M:S"))")
 
     simulated_annealing_run(obj_func=VRPTW.balancing_value_weighted_sum_w6_w4, num_node=i)
     date_finish = now()
